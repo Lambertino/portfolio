94b4d4fbe7647c6092a6f0105e2353aa10e65786
Lambert Roudil <lroudil@toog.fr>
2017-01-16 14:49:57 +0100

    add item into amebat portfolio

--- amebat.php
+++ amebat.php
@@ -35,11 +35,15 @@
 				 	<li data-target="#carousel-project" data-slide-to="1"></li>
 				 	<li data-target="#carousel-project" data-slide-to="2"></li>
 				 	<li data-target="#carousel-project" data-slide-to="3"></li>
+				 	<li data-target="#carousel-project" data-slide-to="4"></li>
 				</ol>
 
 				<!-- Wrapper for slides -->
 				<div class="carousel-inner" role="listbox">
 					<div class="item active">
+						<img src="img/img-web/amebat/amebat-5.jpg" alt="logotype de l'AEMABT illustré sur un bloc-note"/>
+					</div>
+					<div class="item">
 						<img src="img/img-web/amebat/amebat-1.jpg" alt="interface du site de l'AMEBAT sur ordinateur portable"/>
 					</div>
 					<div class="item">
